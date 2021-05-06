345e97831fc325c896cf8a0f6f2ade7f4b96481d
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-05-06 19:51:09 +0300

    finish

--- index.js
+++ index.js
@@ -12,63 +12,44 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-
-
-let idx = 0;
-let idInterval = null;
-
-let mainNumber = () => {
-    for (let i = 0; i < colors.length; i += 1) {
-    
-        idx = randomIntegerFromInterval(0, colors.length-1);
-    
-         console.log(idx);
-        console.log(colors[idx]);
-        bodyEl.style.color = colors[idx];
-        
-        break;
-        
-      
-    };
-};
-
-
- 
-  console.log(idx);
-
- idInterval =  setInterval(mainNumber, 1000,colors);
-console.log(idInterval);
- clearInterval(idInterval);
-
-
-
 const bodyEl = document.querySelector('body');
 const BtnEl = document.querySelectorAll('button');
 const startBtnEl = BtnEl[0];
 const stopBtnEl = BtnEl[1];
 
-console.log(startBtnEl);
-console.log(stopBtnEl);
+let idx = 0;
+let idInterval = null;
 
-const pushStart = () => {
-    bodyEl.classList.add('active');
-    mainNumber();
-}
+const timer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
 
-const pushStop = () => {
-    bodyEl.classList.remove('active')
-    clearInterval(idInterval);
-}
+    this.isActive = true;
+    
+    let mainNumber = () => {
+        
+      idx = randomIntegerFromInterval(0, colors.length - 1);
+      console.log(idx);
+      console.log(colors[idx]);
+      bodyEl.style.backgroundColor = colors[idx];
+                  
+    };
+        
+   this.idInterval = setInterval(mainNumber, 1000);
+  },
+        
+  stop() {
+    this.isActive = false;
+    clearInterval(this.idInterval)
+  },
+};
 
 
 
+startBtnEl.addEventListener('click', timer.start.bind(timer));
 
-
-console.log(bodyEl);
-
-
-    startBtnEl.addEventListener('click', () =>  setInterval(pushStart, 1000));
-
-    stopBtnEl.addEventListener('click', pushStop);
-
+stopBtnEl.addEventListener('click', timer.stop.bind(timer));
 
